c949ce0d3c2cd6c887ad6b6717c0e843a0c23c84
Kouame <kouamedjah@example.com>
2022-04-01 14:06:53 -0500

    First commit

---
+++ src/main/java/com/kouame/demo/Calculator.java
@@ -0,0 +1,18 @@
+package com.kouame.demo;
+
+public class Calculator {
+	  public int add(int i, int j) {
+	        return i + j;
+	    }
+
+	    public int subtract(int i, int j) {
+	        return i - j;
+	    }
+	    public int multiply(int i, int j) {
+	        return i * j;
+	    }
+
+	    public int divide(int i, int j) {
+	        return i / j;
+	    }
+}
---
+++ src/main/java/com/kouame/demo/mainApp.java
@@ -0,0 +1,11 @@
+package com.kouame.demo;
+
+public class mainApp {
+public mainApp() {
+	// TODO Auto-generated constructor stub
+Calculator c=new Calculator();
+    
+    System.out.println("Adding Two Numbers : " +c.add(2, 2));
+}
+}
+
